5f7e7b0c7e4de00da96415d1666573274db44013
Marlon Raskin <marlonjraskin@gmail.com>
2020-07-19 00:31:57 -0400

    Initial Commit

---
+++ Shared/UnsplashTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashTesterApp.swift
+//  Shared
+//
+//  Created by Marlon Raskin on 7/19/20.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
